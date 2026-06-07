097d5f12b63cc9d07443db898673ee403dfcefad
VinFan0 <ryan.beck1820@gmail.com>
2026-06-06 22:23:05 -0600

    feat(drivers): easy set output speed
    Add easy function to set the output speed by passing a port,
    pin, and speed. Speed values labeled as IO_OSPEED_x in io_ospeed
    enum.

--- src/drivers/led.c
+++ src/drivers/led.c
@@ -15,10 +15,10 @@ void led_init(void)
 
     // OPTIONAL: Configure as Push-Pull (default is Push-Pull)
     io_set_otype(IO_PORT_A, 5, IO_OTYPE_PP);
-    // GPIOA->OTYPER &= ~GPIO_OTYPER_OT5;
 
     // OPTIONAL: Set Output speed to High
-    GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED5;
+    io_set_ospeed(IO_PORT_A, 5, IO_OSPEED_LOW);
+    // GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED5;
 }
 
 void led_toggle(void)
