239c050026f9ae82eba1dd5777c80bcf581a2957
VinFan0 <ryan.beck1820@gmail.com>
2026-06-21 17:38:51 -0600

    build: Create main.c example for RTOS, and get working
    Create an example main.c file that works as a FreeRTOS project on
    its own. This will allow me to verify I have FreeRTOS correctly
    configured for the project, and I can use it as a basis for
    converting the superloop blinky into a FreeRTOS blinky.
    
    Provide a stripped down FreeRTOSConfig.h that only holds what is
    needed for the project. This can be edited as test features are
    added and more defines become relevant.
    
    Delete much of the unneccesary FreeRTOS repository files, largely
    because the compiler gets confused by the duplicate files. Will
    need to figure out how I want to store these files in the blinky
    project.

--- src/main.c
+++ src/main.c
@@ -1,132 +1,76 @@
-
-
-#include <stdint.h>
-#include <string.h>
 #include "stm32l476xx.h"
-#include "common/utils.h"
-#include "drivers/io.h"
-
-// FreeRTOS includes
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "timers.h"
 
-/* TEST OPTIONS
- * Comment-in any features or peripherals
- * that you want to test. Any combination
- * of features is intended to work together.
- */
-#define __LED_TEST
-// #define __UART_TEST
-// #define __LCD_TEST
-// #define __SPI_OLED_TEST
+#define LED_PIN 5U
 
-#ifdef __LED_TEST
-#include "drivers/led.h"
-#define LED_PIN (5)
-#endif // __LED_TEST
-
-#ifdef __UART_TEST
-#include "drivers/uart.h"
-#endif // __UART_TEST
-
-#ifdef __LCD_TEST
-#include "drivers/lcd.h"
-#endif // __LCD_TEST
-
-#ifdef __SPI_OLED_TEST
-#include "drivers/spi1.h"
-#include "drivers/oled.h"
-#endif // __SPI_OLED_TEST
-
-#define DELAY_COUNT (1000) // Loop interval in milliseconds
-
-int main(void)
+static void blink_task(void *arg)
 {
-    // clang-format off
-    #ifdef __LED_TEST
-    led_init();
-    #endif // __LED_TEST
-
-  
-
-    #ifdef __UART_TEST
-    uart_init();
-
-        #ifdef __LCD_TEST // UART/LCD_TEST
-    	static uint8_t lcd_char_idx = 0;
-    	#endif // UART/LCD_TEST
-    
-    #endif // __UART_TEST
-
-
-
-    #ifdef __LCD_TEST
-    lcd_init();
-    lcd_transmit_string("Hello World!", sizeof("Hello World!") - 1);
-    lcd_set_cursor(1, 0);
-    #endif // __LCD_TEST
-
-
-
-    #ifdef __SPI_OLED_TEST
-    oled_init();
-    #endif // __SPI_OLED_TEST
-
-
-
-    while (1) {
-	#ifdef __UART_TEST
-        while (uart_data_available()) {
-            char c = uart_read_char();
-            uart_send_char(c);
-            uart_transmit("\r\n");
-
-	    #ifdef __LCD_TEST
-            lcd_send_data(c);
-            if (lcd_char_idx == 15) {
-                lcd_char_idx = 0;
-                lcd_set_cursor(1, 0);
-            } else {
-                lcd_char_idx++;
-            }
-	    #endif // UART/LCD_TEST
-        }
-	#endif // __UART_TEST
-
-
-
-        #ifdef __LED_TEST
-        led_toggle();
-	#endif // __LED_TEST
-
-
-
-	#ifdef __UART_TEST
-        uart_transmit("Sending test string!\r\n");
-	#endif // __UART_TEST
-        // clang-format on
-
-#ifdef __SPI_OLED_TEST
-        oled_clear();
-        oled_flush();
-        delay_ms(500);
-        for (int i = 0; i < 64; i++) {
-            oled_set_pixel(i + 32, 16, 1);
-        }
-        oled_flush();
-#endif // __SPI_OLED_TEST
-
-        delay_ms(DELAY_COUNT);
+    (void)arg;
+    /* Direct register access — no clock_init needed, GPIOA already enabled */
+    for (;;) {
+        GPIOA->ODR ^= (1U << LED_PIN);
+        vTaskDelay(500);
     }
 }
 
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+void vApplicationIdleHook(void) { }
+void vApplicationMallocFailedHook(void)
 {
-    (void)xTask;
-    (void)pcTaskName;
-    // TODO: Stack overflow detected - halt or log
-    while (1)
-        ;
+    for (;;) { }
+}
+void vApplicationStackOverflowHook(TaskHandle_t t, char *n)
+{
+    (void)t;
+    (void)n;
+    for (;;) { }
+}
+void HardFault_Handler(void)
+{
+    for (;;) {
+        GPIOA->ODR ^= (1U << LED_PIN);
+        for (volatile uint32_t d = 0; d < 100000; d++) { }
+    }
+}
+void MemManage_Handler(void)
+{
+    for (;;) {
+        GPIOA->ODR ^= (1U << LED_PIN);
+        for (volatile uint32_t d = 0; d < 30000; d++) { }
+    }
+}
+void BusFault_Handler(void)
+{
+    for (;;) {
+        GPIOA->ODR ^= (1U << LED_PIN);
+        for (volatile uint32_t d = 0; d < 150000; d++) { }
+    }
+}
+void UsageFault_Handler(void)
+{
+    for (;;) {
+        GPIOA->ODR ^= (1U << LED_PIN);
+        for (volatile uint32_t d = 0; d < 300000; d++) { }
+    }
+}
+
+int main(void)
+{
+    /* Enable GPIOA clock */
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+    (void)RCC->AHB2ENR;
+
+    /* Enable fault handlers */
+    SCB->SHCSR |= SCB_SHCSR_USGFAULTENA_Msk | SCB_SHCSR_BUSFAULTENA_Msk | SCB_SHCSR_MEMFAULTENA_Msk;
+
+    /* PA5 output */
+    GPIOA->MODER = (GPIOA->MODER & ~(3U << (LED_PIN * 2))) | (1U << (LED_PIN * 2));
+    GPIOA->OTYPER &= ~(1U << LED_PIN);
+    GPIOA->PUPDR &= ~(3U << (LED_PIN * 2));
+    GPIOA->OSPEEDR &= ~(3U << (LED_PIN * 2));
+
+    xTaskCreate(blink_task, "b", 256, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+    for (;;) { }
 }
