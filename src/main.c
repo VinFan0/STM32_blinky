541c078edcf4610c6b999eda31b19cc35ba3ad99
VinFan0 <ryan.beck1820@gmail.com>
2026-06-22 21:39:32 -0600

    feat(RTOS): Add test feature skeleton
    The LED blink and UART test features have been added to the project.
    Incorporate a similar system of defines to the superloop project to
    select which test features to enable.
    
    Move the include for led.h and the led_init function into the ifdef
    for the LED_TEST feature. This change also requires removing the
    macros and wrapper functions for the LED control from the various
    fault handlers.

--- src/main.c
+++ src/main.c
@@ -5,9 +5,20 @@
 #include <string.h>
 #include "common/utils.h"
 #include "drivers/io.h"
-#include "drivers/led.h"
-#include "drivers/uart.h"
 
+/* TEST OPTIONS
+ * Comment-in any features or peripherals
+ * that you want to test. Any combination
+ * of features is intended to work together.
+ */
+#define LED_TEST
+#define UART_TEST
+// #define LCD_TEST
+// #define SPI_OLED_TEST
+
+/* LED_TEST ****************************************************/
+#ifdef LED_TEST
+#include "drivers/led.h"
 #define LED_PIN 5U
 
 static void blink_task(void *arg)
@@ -19,28 +30,48 @@ static void blink_task(void *arg)
         vTaskDelay(250);
     }
 }
+#endif // LED_TEST
+
+/* UART_TEST ***************************************************/
+#ifdef UART_TEST
+#include "drivers/uart.h"
 
 static void uart_transmit_task(void *arg)
 {
     (void)arg;
     for (;;) {
-	uart_transmit("Sending test string!\r\n");
-	vTaskDelay(1000);
+        uart_transmit("Sending test string!\r\n");
+        vTaskDelay(1000);
     }
 }
+#endif // UART_TEST
+
+/* LCD_TEST ****************************************************/
+#ifdef LCD_TEST
+#include "drivers/lcd.h"
+#endif // LCD_TEST
+
+/* SPI_OLED_TEST ***********************************************/
+#ifdef SPI_OLED_TEST
+#include "drivers/spi1.h"
+#include "drivers/oled.h"
+#endif // SPI_OLED_TEST
 
 int main(void)
 {
 
+#ifdef LED_TEST
     led_init();
+    xTaskCreate(blink_task, "b", 256, NULL, 1, NULL);
+#endif // LED_TEST
 
+#ifdef UART_TEST
     uart_init();
+    xTaskCreate(uart_transmit_task, "b", 256, NULL, 1, NULL);
+#endif // UART_TEST
 
     /* Enable fault handlers */
     SCB->SHCSR |= SCB_SHCSR_USGFAULTENA_Msk | SCB_SHCSR_BUSFAULTENA_Msk | SCB_SHCSR_MEMFAULTENA_Msk;
-
-    xTaskCreate(blink_task, "b", 256, NULL, 1, NULL);
-    xTaskCreate(uart_transmit_task, "b", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
     for (;;) { }
@@ -52,8 +83,8 @@ void vApplicationMallocFailedHook(void)
 {
     // TODO: ASSERT(0)
     for (;;) {
-        GPIOA->ODR ^= (1U << LED_PIN);
-        for (volatile uint32_t d = 0; d < 50000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 20000; d++) { }
     }
 }
 void vApplicationStackOverflowHook(TaskHandle_t t, char *n)
@@ -62,39 +93,39 @@ void vApplicationStackOverflowHook(TaskHandle_t t, char *n)
     (void)n;
     // TODO: ASSERT(0)
     for (;;) {
-	    GPIOA->ODR ^= (1U << LED_PIN);
-	    for(volatile uint32_t d=0; d<300000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 300000; d++) { }
     }
 }
 void HardFault_Handler(void)
 {
     // TODO: ASSERT(0)
     for (;;) {
-	    GPIOA->ODR ^= (1U << LED_PIN);
-	    for(volatile uint32_t d=0; d<400000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 400000; d++) { }
     }
 }
 void MemManage_Handler(void)
 {
     // TODO: ASSERT(0)
     for (;;) {
-	    GPIOA->ODR ^= (1U << LED_PIN);
-	    for(volatile uint32_t d=0; d<500000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 500000; d++) { }
     }
 }
 void BusFault_Handler(void)
 {
     // TODO: ASSERT(0)
     for (;;) {
-	    GPIOA->ODR ^= (1U << LED_PIN);
-	    for(volatile uint32_t d=0; d<600000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 600000; d++) { }
     }
 }
 void UsageFault_Handler(void)
 {
     // TODO: ASSERT(0)
     for (;;) {
-	    GPIOA->ODR ^= (1U << LED_PIN);
-	    for(volatile uint32_t d=0; d<700000; d++) { }
+        GPIOA->ODR ^= (1U << 5);
+        for (volatile uint32_t d = 0; d < 700000; d++) { }
     }
 }
